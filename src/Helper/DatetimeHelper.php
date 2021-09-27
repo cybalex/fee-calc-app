2dba955daf4e9c21deb9ec421fcb5c4a52b47cf5
Oleksii Zymovets <o.zymovets@andersenlab.com>
2021-09-27 10:57:52 +0300

    ISO-8601 datetime check for week interval

--- src/Helper/DatetimeHelper.php
+++ src/Helper/DatetimeHelper.php
@@ -4,21 +4,12 @@ declare(strict_types=1);
 
 namespace FeeCalcApp\Helper;
 
-use DateInterval;
+use DateTime;
 
 class DatetimeHelper
 {
-    public function datesAreWithinSameWeek(\DateTime $date1, \DateTime $date2): bool
+    public function datesAreWithinSameWeek(DateTime $date1, DateTime $date2): bool
     {
-        $daysAfterMonday = (int) $date1->format('N') - 1;
-        $daysBeforeSunday = 7 - (int) $date1->format('N');
-
-        $prevMonday = clone $date1;
-        $prevMonday->sub(new DateInterval('P'.$daysAfterMonday.'D'))->setTime(0, 0, 0, 0);
-
-        $nextSunday = clone $date1;
-        $nextSunday->add(new DateInterval('P'.$daysBeforeSunday.'D'))->setTime(23, 59, 59, 999);
-
-        return $date2 >= $prevMonday && $date2 <= $nextSunday;
+        return $date1->format('oW') === $date2->format('oW');
     }
 }
